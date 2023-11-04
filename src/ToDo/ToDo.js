fe6aea42e02fdf50d23d8d7afc3c6bd4b6d2b7fb
Prim Asghar <mirpasghar@gmail.com>
2023-11-04 21:57:53 +0200

    Added border radius for round corners, Changed padding of button, Removed the shapes from the bottom.

--- src/ToDo/ToDo.js
+++ src/ToDo/ToDo.js
@@ -138,32 +138,6 @@ function ToDo() {
           />
         </section>
       </main>
-      {/*<div className={classes.shapeBottom}>*/}
-      {/*  <svg*/}
-      {/*    data-name="Layer 1"*/}
-      {/*    xmlns="http://www.w3.org/2000/svg"*/}
-      {/*    viewBox="0 0 1200 120"*/}
-      {/*    preserveAspectRatio="none"*/}
-      {/*  >*/}
-      {/*    <path*/}
-      {/*      d="M892.25 114.72L0 0 0 120 1200 120 1200 0 892.25 114.72z"*/}
-      {/*      className="shape-fill"*/}
-      {/*    ></path>*/}
-      {/*  </svg>*/}
-      {/*</div>*/}
-      <div className={classes.triAngle}>
-        <svg
-          data-name="Layer 1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1200 120"
-          preserveAspectRatio="none"
-        >
-          <path
-            d="M598.97 114.72L0 0 0 120 1200 120 1200 0 598.97 114.72z"
-            className="shape-fill"
-          ></path>
-        </svg>
-      </div>
     </div>
   );
 }
