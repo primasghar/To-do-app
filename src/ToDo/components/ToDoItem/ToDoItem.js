56375306f97b69bf787ebb421b967d9a0159e0df
Prim Asghar <mirpasghar@gmail.com>
2023-11-11 01:05:51 +0200

    Added shuffle icon and linked the shuffle list function with it.

--- src/ToDo/components/ToDoItem/ToDoItem.js
+++ src/ToDo/components/ToDoItem/ToDoItem.js
@@ -3,14 +3,24 @@ import classes from "./ToDoItem.module.css";
 
 import {Icon} from "react-icons-kit";
 import {remove} from 'react-icons-kit/fa/remove'
+import {longArrowLeft} from 'react-icons-kit/fa/longArrowLeft';
+import {longArrowRight} from 'react-icons-kit/fa/longArrowRight'
 
-const ToDoItem = ({ text, handleTask, handleRemoval}) => {
-  return (
-    <li className={classes.item} onClick={handleTask} role="button">
-      <p>{text}</p>
-        <Icon icon={remove}   className={classes.del} onClick={handleRemoval}/>
-    </li>
-  );
+const ToDoItem = ({text, handleTask, handleRemoval}) => {
+
+    return (
+        <li className={classes.item}>
+            <p>{text}</p>
+            <span className={classes.itemButtons}>
+                <Icon icon={remove} className={classes.delBtn} onClick={handleRemoval} role="button"/>
+                <span className={classes.shuffle} onClick={handleTask}  role="button">
+                    <Icon icon={longArrowLeft} className={classes.arrowLeft } size={16}/>
+                    <Icon icon={longArrowRight} className={classes.arrowRight} size={16}/>
+                </span>
+
+            </span>
+        </li>
+    );
 };
 
 export default ToDoItem;
